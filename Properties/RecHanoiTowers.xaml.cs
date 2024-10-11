4e47debe688d79c6a7377da34f07aae41c1de365
Sergo Trofimov <150113264+SergeyTrofi@users.noreply.github.com>
2024-10-12 03:31:08 +0500

    InitCommit

---
+++ Properties/RecHanoiTowers.xaml.cs
@@ -0,0 +1,31 @@
+﻿using HanoiTower;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
+
+namespace LaboratoryP2.Properties
+{
+    /// <summary>
+    /// Логика взаимодействия для RecHanoiTowers.xaml
+    /// </summary>
+    public partial class RecHanoiTowers : Window
+    {
+        private static string _movesCount;
+
+        public RecHanoiTowers()
+        {
+            InitializeComponent();
+        } 
+    }
+}
---
+++ RecHanoiTower.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace HanoiTower
+{
+    public class RecHanoiTower
+    {
+        private Stack<int> _sourcePole;
+        private Stack<int> _destinationPole;
+        private Stack<int> _withPole;
+        public int _movesCount;
+
+        public RecHanoiTower(int numberOfDisks)
+        {
+            _sourcePole = new Stack<int>();
+            _destinationPole = new Stack<int>();
+            _withPole = new Stack<int>();
+
+            for (int i = numberOfDisks; i > 0; i--)
+            {
+                _sourcePole.Push(i);
+            }
+        }
+
+        public int Solve()
+        {
+            _movesCount = 0;
+            MoveDisks(_sourcePole.Count, _sourcePole, _destinationPole, _withPole);
+            return _movesCount;
+        }
+
+        private void MoveDisks(int numberOfDisks, Stack<int> sourcePole, Stack<int> destinationPole, Stack<int> withPole)
+        {
+            if (numberOfDisks == 1)
+            {
+                // Если количество дисков равно 1, переносим диск на целевую башню
+                destinationPole.Push(sourcePole.Pop());
+                _movesCount++;
+                Console.WriteLine("Количество ходов: " + _movesCount);
+            }
+            else
+            {
+                // Рекурсивный вызов для переноса всех дисков кроме самого маленького
+                MoveDisks(numberOfDisks - 1, sourcePole, withPole, destinationPole);
+
+                // Перенос самого маленького диска на целевую башню
+                destinationPole.Push(sourcePole.Pop());
+                _movesCount++;
+                Console.WriteLine("Количество ходов: " + _movesCount);
+
+                // Рекурсивный вызов для переноса оставшихся дисков на целевую башню
+                MoveDisks(numberOfDisks - 1, withPole, destinationPole, sourcePole);
+            }
+        }
+    }
+}
